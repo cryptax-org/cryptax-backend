595269bf12e1cb9a5ce0df8b819846871d47964b
CarlPhilipp Harmant <cp.harmant@gmail.com>
2018-08-13 22:45:42 -0500

    Fix last push

--- scripts/create-docker-image.sh
+++ scripts/create-docker-image.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 set -ev
-tag=$(if [ "$TRAVIS_BRANCH" == "develop" ]; then echo "$TRAVIS_BRANCH-$TRAVIS_COMMIT"; elif [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; fi)
-if [ "$TRAVIS_BRANCH" == "develop" ] || [ "$TRAVIS_BRANCH" == "master" ]; then
-    docker build    --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
-                    --build-arg COMMIT_ID=$TRAVIS_COMMIT \
-                    --tag cryptaxapp/cryptax-backend:$tag .
-fi
-#if [ "$TRAVIS_BRANCH" == "develop" ]; then
-#    docker build --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
-#                 --build-arg COMMIT_ID=$TRAVIS_COMMIT \
-#                 --tag cryptaxapp/cryptax-backend:$TRAVIS_BRANCH-$TRAVIS_COMMIT .
-#elif [ "$TRAVIS_BRANCH" == "master" ]; then
-#    docker build --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
-#                 --build-arg COMMIT_ID=$TRAVIS_COMMIT \
-#                 --tag cryptaxapp/cryptax-backend:latest .
-#fi
+tag=$(if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo "$TRAVIS_BRANCH-$TRAVIS_COMMIT"; fi)
+docker build    --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
+                --build-arg COMMIT_ID=$TRAVIS_COMMIT \
+                --tag cryptaxapp/cryptax-backend:$tag .
