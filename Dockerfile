11ca3011701d865957a426970a5c10ffa35b4c0e
CarlPhilipp Harmant <cp.harmant@gmail.com>
2018-08-13 22:19:05 -0500

    Improve tag

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,13 @@ FROM openjdk:10-jdk-slim
 
 MAINTAINER Carl-Philipp Harmant <cp.harmant@gmail.com>
 
+ARG BUILD_DATE
+ARG COMMIT_ID
+
+LABEL com.cryptax.build-date=$BUILD_DATE \
+      com.cryptax.name="Cryptax Backend" \
+      com.cryptax.commit-id=$COMMIT_ID \
+
 ## Build the app
 COPY . /tmp/cryptax
 WORKDIR /tmp/cryptax
---
+++ scripts/create-docker-image.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -ev
+
+tag=$(if [ "$TRAVIS_BRANCH" == "develop" ]; then echo "$TRAVIS_BRANCH-$TRAVIS_COMMIT"; elif [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; fi)
+
+if [ "$TRAVIS_BRANCH" == "develop" ] || [ "$TRAVIS_BRANCH" == "master" ]; then
+    docker build    --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
+                    --build-arg COMMIT_ID=$TRAVIS_COMMIT \
+                    --tag cryptaxapp/cryptax-backend:$tag .
+fi
+#if [ "$TRAVIS_BRANCH" == "develop" ]; then
+#    docker build --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
+#                 --build-arg COMMIT_ID=$TRAVIS_COMMIT \
+#                 --tag cryptaxapp/cryptax-backend:$TRAVIS_BRANCH-$TRAVIS_COMMIT .
+#elif [ "$TRAVIS_BRANCH" == "master" ]; then
+#    docker build --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
+#                 --build-arg COMMIT_ID=$TRAVIS_COMMIT \
+#                 --tag cryptaxapp/cryptax-backend:latest .
+#fi
