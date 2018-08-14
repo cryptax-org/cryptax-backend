8a636cb0d52427a9b73dbcd242e376381704c60d
CarlPhilipp Harmant <cp.harmant@gmail.com>
2018-08-13 19:30:47 -0500

    New try

--- scripts/travis-push-image.sh
+++ scripts/travis-push-image.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -ev
-bundle exec rake:units
 if [[ "$TRAVIS_BRANCH" == "develop" ]]; then
     docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD ;
     docker push cryptaxapp/cryptax-backend:latest ;
