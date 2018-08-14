2b73f6bd39d449f0a3897e44bf06f8fed0c11801
CarlPhilipp Harmant <cp.harmant@gmail.com>
2018-08-13 23:08:27 -0500

    Improve docker tag

--- scripts/create-docker-image.sh
+++ scripts/create-docker-image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ev
-tag=$(if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo "$TRAVIS_BRANCH-$TRAVIS_COMMIT"; fi)
+tag=$(if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; elif [ "$TRAVIS_BRANCH" == "develop" ]; then echo "develop-latest"; else echo "$TRAVIS_BRANCH-$TRAVIS_COMMIT"; fi)
 docker build    --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
                 --build-arg COMMIT_ID=$TRAVIS_COMMIT \
                 --tag cryptaxapp/cryptax-backend:$tag .
