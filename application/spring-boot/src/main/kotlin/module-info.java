6aef256809e1cec5a3e247f683a290dcdb51f164
Carl-Philipp Harmant <cp.harmant@gmail.com>
2018-09-11 17:00:34 -0500

    Fix module dep

--- application/spring-boot/src/main/kotlin/module-info.java
+++ application/spring-boot/src/main/kotlin/module-info.java
@@ -1,39 +1,38 @@
 open module cryptax.spring.app {
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.datatype.jsr310;
+    requires com.fasterxml.jackson.module.kotlin;
+
+    requires cryptax.controller;
     requires cryptax.config.properties;
+    requires cryptax.domain;
+    requires cryptax.db.simple;
+    requires cryptax.db.cloud.datastore;
+    requires cryptax.email;
+    requires cryptax.id;
+    requires cryptax.security;
+    requires cryptax.usecase;
+
+    requires google.cloud.datastore;
+    requires google.auth.library.oauth2.http;
+
+    requires io.reactivex.rxjava2;
+    requires java.validation;
+    requires jjwt;
     requires kotlin.stdlib;
+    requires okhttp3;
     requires org.slf4j;
+    requires reactor.core;
+
+    requires spring.core;
     requires spring.boot.autoconfigure;
     requires spring.boot;
     requires spring.context;
     requires spring.beans;
-    requires spring.web;
-    requires cryptax.controller;
-    requires cryptax.usecase;
-    requires cryptax.domain;
-    requires cryptax.db.simple;
-    requires cryptax.security;
-    requires cryptax.id;
-    requires okhttp3;
-    requires cryptax.email;
-    requires cryptax.db.cloud.datastore;
-    requires google.cloud.datastore;
-    requires google.auth.library.oauth2.http;
-    requires com.fasterxml.jackson.databind;
-    //requires com.fasterxml.jackson.annotation;
-    requires com.fasterxml.jackson.datatype.jsr310;
-    requires com.fasterxml.jackson.module.kotlin;
-    requires com.fasterxml.jackson.core;
-    requires io.reactivex.rxjava2;
-    //requires java.validation;
-    requires spring.security.config;
-    requires jjwt;
     requires spring.security.core;
-    requires spring.core;
+    requires spring.security.config;
     requires spring.security.web;
+    requires spring.web;
     requires spring.webflux;
-    //requires reactor.core;
-    //requires reactor.adapter;
-    //requires tomcat.embed.core;
-    //requires spring.webmvc;
-    //requires java.xml.ws.annotation;
 }
